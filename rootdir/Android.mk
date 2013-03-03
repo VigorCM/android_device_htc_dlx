c242003f69a217a6bdad2fa17fcb4c362b353d43
boss <boss@boss-Inspiron-1545.(none)>
2013-03-02 17:55:44 -0800

    forgot this lol thanks flyhalf

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,10 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= fstab.jet
+LOCAL_MODULE		:= fstab.dlx
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/fstab.jet
+LOCAL_SRC_FILES		:= etc/fstab.dlx
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.qcom.firmware_links.sh
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.qcom.firmware_links.sh
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
